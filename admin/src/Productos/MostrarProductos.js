1bac700e9fb0283b719c603ff89c88f9ba7c99bb
Colibri <colibri@Sangrael.ipv6.infinitum.net.mx>
2024-10-08 22:40:05 -0600

    Interfaces para admin

--- admin/src/Productos/MostrarProductos.js
+++ admin/src/Productos/MostrarProductos.js
@@ -9,6 +9,8 @@ async function mostrar_productos() {
         const response = await fetch('/auth/mostrarProductos')
         const data = await response.json()
 
+        const cantidad = document.querySelector('.cantidad')
+        cantidad.textContent = `(${data.length})`
 
         data.forEach(element => {
             console.log(element);
@@ -28,27 +30,55 @@ async function mostrar_productos() {
                 estado = 'Preventa'
 
             }
-            
-            const carta = `
-            <div id="${element.id}" class="cart">
-                <label class="checkBox_filtro"><input name="radio" type="checkbox"><div class="transition_checkbox"></div></label>
-                <img src="../img/articulos/${element.id}.png" alt="alt"/>
-                <div class="contendor_cart_nombre">
-                    <h2>${element.descripcion}</h2>
+
+            let carta
+
+            if (window.location.pathname == '/productos') {
+
+                carta = `
+                <div id="${element.id}" class="cart">
+                    <label class="checkBox_filtro"><input name="radio" type="checkbox"><div class="transition_checkbox"></div></label>
+                    <img src="../img/articulos/${element.id}.png" alt="alt"/>
+                    <div class="contendor_cart_nombre">
+                        <h2>${element.descripcion}</h2>
+                    </div>
+                    <h2>${element.variantes} variantes</h2>
+                    <h2>${element.id}000000000M</h2>
+                    <h2>$ ${element.precio} mxn</h2>
+                    <h2>${estado}</h2>
+                        
+                    <svg xmlns="http://www.w3.org/2000/svg" onclick="cambiarClase_eliminar(${element.id})" width="24" height="24" viewBox="0 0 24 24"   fill="#6F6D6D">
+                        <mask id="mask0_2039_18386" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                        <rect width="24" height="24" />
+                        </mask>
+                        <path d="M6.93398 21.2033C6.30432 21.2033 5.76773 20.9816 5.32423 20.5381C4.88073 20.0946 4.65898 19.558 4.65898 18.9283V6.06582H3.52148V3.79082H8.86223V2.65332H15.1252V3.79082H20.478V6.06582H19.3405V18.9283C19.3405 19.558 19.1187 20.0946 18.6752 20.5381C18.2317 20.9816 17.6952 21.2033 17.0655 21.2033H6.93398ZM17.0655 6.06582H6.93398V18.9283H17.0655V6.06582ZM8.89223 16.9941H11.0297V7.99407H8.89223V16.9941ZM12.9697 16.9941H15.1072V7.99407H12.9697V16.9941Z" />
+                    </svg>
                 </div>
-                <h2>${element.variantes} variantes</h2>
-                <h2>${element.id}</h2>
-                <h2>$ ${element.precio} mxn</h2>
-                <h2>${estado}</h2>
+                `
+            } else if (window.location.pathname == '/inventario') {
+
+                carta = `
+                    <div class="cart">
+                        <label class="checkBox_filtro"><input name="radio" type="checkbox"><div class="transition_checkbox"></div></label>
+                        <img src="../../img/articulos/${element.id}.png" alt="alt"/>
+                        <div class="contendor_cart_nombre">
+                            <h2>${element.descripcion}</h2>
+                        </div>
+                        <h2>${element.variantes} variantes</h2>
+                        <h2>${element.id}000000000M</h2>
+                        <h2>${element.estado}</h2>
                        
-                <svg xmlns="http://www.w3.org/2000/svg" onclick="cambiarClase_eliminar(${element.id})" width="24" height="24" viewBox="0 0 24 24"   fill="#6F6D6D">
-                    <mask id="mask0_2039_18386" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
-                    <rect width="24" height="24" />
-                    </mask>
-                    <path d="M6.93398 21.2033C6.30432 21.2033 5.76773 20.9816 5.32423 20.5381C4.88073 20.0946 4.65898 19.558 4.65898 18.9283V6.06582H3.52148V3.79082H8.86223V2.65332H15.1252V3.79082H20.478V6.06582H19.3405V18.9283C19.3405 19.558 19.1187 20.0946 18.6752 20.5381C18.2317 20.9816 17.6952 21.2033 17.0655 21.2033H6.93398ZM17.0655 6.06582H6.93398V18.9283H17.0655V6.06582ZM8.89223 16.9941H11.0297V7.99407H8.89223V16.9941ZM12.9697 16.9941H15.1072V7.99407H12.9697V16.9941Z" />
-                </svg>
-            </div>
-            `
+                        <svg xmlns="http://www.w3.org/2000/svg" onclick="cambiarClase_eliminar(${element.id})" width="24" height="24" viewBox="0 0 24 24"   fill="#6F6D6D">
+                            <mask id="mask0_2039_18386" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                            <rect width="24" height="24" />
+                            </mask>
+                            <path d="M6.93398 21.2033C6.30432 21.2033 5.76773 20.9816 5.32423 20.5381C4.88073 20.0946 4.65898 19.558 4.65898 18.9283V6.06582H3.52148V3.79082H8.86223V2.65332H15.1252V3.79082H20.478V6.06582H19.3405V18.9283C19.3405 19.558 19.1187 20.0946 18.6752 20.5381C18.2317 20.9816 17.6952 21.2033 17.0655 21.2033H6.93398ZM17.0655 6.06582H6.93398V18.9283H17.0655V6.06582ZM8.89223 16.9941H11.0297V7.99407H8.89223V16.9941ZM12.9697 16.9941H15.1072V7.99407H12.9697V16.9941Z" />
+                        </svg>
+                    </div>
+                `
+            }
+            
+            
             
              lista.innerHTML += carta
         });
